3c677d0651c5bd25cf6076feca7a22ad1bf98433
Justin ThinkPad <justin.connolly@gmail.com>
2022-06-20 22:09:48 -0400

    LC 1952

---
+++ 1952-threeDivisors-Easy.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isThree(self, n: int) -> bool:
+        i = 1
+        divisors = 1
+        while i <= n//2:   
+            if (n % i == 0) :
+                print(f"{i} divides {n}")
+                divisors += 1
+            i = i + 1
+            if divisors > 3:
+                return False
+        if divisors == 3:
+            return True
+        return False
+
+sol = Solution()
+print(sol.isThree(9))
